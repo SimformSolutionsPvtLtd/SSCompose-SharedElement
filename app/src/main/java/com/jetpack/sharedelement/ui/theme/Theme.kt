a294eec6b34c26ba7b4ff5ffc8462f1db24eca8c
Aparna Tati <aparna.t@simformsolutions.com>
2024-07-10 19:28:42 +0530

    Project Setup

---
+++ app/src/main/java/com/jetpack/sharedelement/ui/theme/Theme.kt
@@ -0,0 +1,54 @@
+package com.jetpack.sharedelement.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkPinkColorPalette = darkColors(
+    primary = pink200,
+    secondary = teal200,
+    background = Color.Black,
+    surface = Color.Black,
+    onPrimary = Color.Black,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    error = Color.Red,
+)
+
+private val LightPinkColorPalette = lightColors(
+    primary = pink500,
+    secondary = teal200,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
+@Composable
+fun SharedElementTransitionTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            if (darkTheme) DarkPinkColorPalette else LightPinkColorPalette
+        }
+
+        darkTheme -> DarkPinkColorPalette
+        else -> LightPinkColorPalette
+    }
+
+    MaterialTheme(
+        colors = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
