43d10b659f38889cc27b6240d5a5c8f6f50c57fb
Aparna Tati <aparna.t@simformsolutions.com>
2024-07-10 19:28:42 +0530

    Project Setup

---
+++ app/src/main/java/com/jetpack/sharedelement/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.jetpack.sharedelement.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val pink200 = Color(0XFFF27584)
+val pink500 = Color(0XFFEF5366)
+val pink700 = Color(0XFFD74A5B)
+
+val teal200 = Color(0XFF80DEEA)
